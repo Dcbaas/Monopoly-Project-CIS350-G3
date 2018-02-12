38af73a1082c5783ed414ee06c77b3e4436d36d1
David <bcdave.301@gmail.com>
2018-02-12 15:22:12 -0500

    Added Comments

--- Model/src/BoardPackage/PropertySquare.java
+++ Model/src/BoardPackage/PropertySquare.java
@@ -52,6 +52,22 @@ public class PropertySquare extends BoardSquare{
     /**An int to track the cost of purchasing one hotel for this property*/
     private final int hotelCost;
 
+    /******************************************************************
+     * The constructor creates a Property and sets all the attributes
+     * related to the property.
+     * @param name the name of the PropertySquare
+     * @param price the price of the property
+     * @param mortgageValue The mortgage value of the property.
+     * @param baseRent The rent cost of the property.
+     * @param oneHouseRent The rent cost of the property w/ one house.
+     * @param twoHouseRent The rent cost of the property w/ two houses.
+     * @param threeHouseRent The rent cost of the property w/ three houses.
+     * @param fourHouseRent The rent cost of the property w/ four houses.
+     * @param hotelRent The rent cost of the property w/ a hotel.
+     * @param numHouses The number of houses on the property.
+     * @param houseCost The cost of adding a house to the property.
+     * @param hotelCost The cost of adding a hotel to the property.
+     *****************************************************************/
     public PropertySquare(String name, int price, int mortgageValue,
                           int baseRent, int oneHouseRent,
                           int twoHouseRent, int threeHouseRent,
@@ -79,66 +95,166 @@ public class PropertySquare extends BoardSquare{
         this.hotelCost = hotelCost;
     }
 
+    /******************************************************************
+     * The getPrice method returns the price of this property.
+     *
+     * @return the price of the property.
+     *****************************************************************/
     public int getPrice() {
         return price;
     }
 
+    /******************************************************************
+     * The getMortgageValue method returns the value of mortgaging
+     * this property.
+     *
+     * @return the mortgage value of the property.
+     *****************************************************************/
     public int getMortgageValue() {
         return mortgageValue;
     }
 
+    /******************************************************************
+     * The getBaseRent method returns the cost of rent for this property
+     *
+     * @return The base rent of the property.
+     *****************************************************************/
     public int getBaseRent() {
         return baseRent;
     }
 
+    /******************************************************************
+     * The getOneHouseRent method returns the cost of rent with one
+     * house on the property.
+     *
+     * @return oneHouseRent The cost of rent w/ one house on the
+     * property.
+     *****************************************************************/
     public int getOneHouseRent() {
         return oneHouseRent;
     }
 
+    /******************************************************************
+     * The getTwoHouseRent method returns the cost of rent with two
+     * houses on the property.
+     *
+     * @return twoHouseRent The cost of rent w/ two houses on the
+     * property.
+     *****************************************************************/
     public int getTwoHouseRent() {
         return twoHouseRent;
     }
 
+    /******************************************************************
+     * The getThreeHouseRent method returns the cost of rent with three
+     * houses on the property.
+     *
+     * @return threeHouseRent The cost of rent w/ three houses on the
+     * property.
+     *****************************************************************/
     public int getThreeHouseRent() {
         return threeHouseRent;
     }
 
+    /******************************************************************
+     * The getFourHouseRent method returns the cost of rent with four
+     * houses on the property.
+     *
+     * @return fourHouseRent The cost of rent w/ three houses on the
+     * property.
+     *****************************************************************/
     public int getFourHouseRent() {
         return fourHouseRent;
     }
 
+    /******************************************************************
+     * The getHotelRent method returns the cost of rent with a hotel
+     * on the property.
+     *
+     * @return hotelRent The cost of rent w/ three houses on the
+     * property.
+     *****************************************************************/
     public int getHotelRent() {
         return hotelRent;
     }
 
+    /******************************************************************
+     * The getNumHouses method returns how many houses are on this
+     * property.
+     *
+     * @return numHouses the number of houses on the property.
+     *****************************************************************/
     public int getNumHouses() {
         return numHouses;
     }
 
+    /******************************************************************
+     * The setNumHouses method sets the number of houses on the
+     * property.
+     *
+     * @param numHouses the number of houses for this property.
+     *****************************************************************/
     public void setNumHouses(int numHouses) {
         this.numHouses = numHouses;
     }
 
+    /******************************************************************
+     * The isMortgaged method returns weather the property is
+     * mortgaged or not.
+     *
+     * @return true if the property is mortgaged, false otherwise.
+     *****************************************************************/
     public boolean isMortgaged() {
         return isMortgaged;
     }
 
+    /******************************************************************
+     * The setMortgaged method sets the mortgage flag.
+     *
+     * @param mortgaged the mortgage status this property is being set
+     *                  to.
+     *****************************************************************/
     public void setMortgaged(boolean mortgaged) {
         isMortgaged = mortgaged;
     }
 
+    /******************************************************************
+     * The getOwner method returns the Player who owns this property or
+     * null if the bank owns the property.
+     *
+     * @return owner the Player who owns the property or null if the
+     * bank owns the property.
+     *****************************************************************/
     public Player getOwner() {
         return owner;
     }
 
+    /******************************************************************
+     * The setOwner method sets the Player who owns this property.
+     * set the input to null if the bank should own the property.
+     *
+     * @param owner the Player who will own the property.
+     *****************************************************************/
     public void setOwner(Player owner) {
         this.owner = owner;
     }
 
+    /******************************************************************
+     * The getHouseCost method returns the cost of purchasing a house
+     * on this property.
+     *
+     * @return houseCost the cost of purchasing a house on this property.
+     *****************************************************************/
     public int getHouseCost() {
         return houseCost;
     }
 
+    /******************************************************************
+     * The getHotelCost method returns the cost of purchasing a Hotel
+     * on this property.
+     *
+     * @return hotelCost the cost of purchasing a Hotel on this property.
+     *****************************************************************/
     public int getHotelCost() {
         return hotelCost;
     }
