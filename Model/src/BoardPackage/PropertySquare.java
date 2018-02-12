53cc778f35b6aa6a6c16368e1fb32940057ba369
David <bcdave.301@gmail.com>
2018-02-11 19:04:55 -0500

    Added Instance var comments

--- Model/src/BoardPackage/PropertySquare.java
+++ Model/src/BoardPackage/PropertySquare.java
@@ -13,30 +13,43 @@ import GamePackage.Player;
  ********************************************************************/
 public class PropertySquare extends BoardSquare{
 
+    /**An int to hold the price of the property*/
     private final int price;
 
+    /**An int to hold the property's mortgage value*/
     private final int mortgageValue;
 
+    /**An int to hold the cost of rent w/ no houses.*/
     private final int baseRent;
 
+    /**An int to hold the cost of rent w/ one house*/
     private final int oneHouseRent;
 
+    /**An int to hold the cost of rent w/ two houses*/
     private final int twoHouseRent;
 
+    /**An int to hold the cost of rent w/ three houses*/
     private final int threeHouseRent;
 
+    /**An int to hold the cost of rent w/ four houses*/
     private final int fourHouseRent;
 
+    /**An int to hold the cost of rent w/ a hotel*/
     private final int hotelRent;
 
+    /**An int to track how many houses are on the property*/
     private int numHouses;
 
+    /**A boolean flag to track if the property is mortgaged*/
     private boolean isMortgaged;
 
+    /**A Player variable to track what player owns this property*/
     private Player owner;
 
+    /**An int to track the cost of purchasing one house for this property*/
     private final int houseCost;
 
+    /**An int to track the cost of purchasing one hotel for this property*/
     private final int hotelCost;
 
     public PropertySquare(String name, int price, int mortgageValue,
