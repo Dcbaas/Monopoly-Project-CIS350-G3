8ea9788f5f15696a6feb6a3c2b3f31345e4c99a5
David <bcdave.301@gmail.com>
2018-02-15 15:39:10 -0500

    Wrote ParkingSquareClass

--- Model/src/BoardPackage/FreeParkingSquare.java
+++ Model/src/BoardPackage/FreeParkingSquare.java
@@ -1,7 +1,20 @@
 package BoardPackage;
 
-/**
- * Created by Dylan Kernohan on 2/10/18.
+/*********************************************************************
+ * The FreeParkingSquare is a board square on the Monopoly game board
+ * where when you land on it nothing happens. This class exist however
+ * for the possiblity of adding functionality for this board square
+ * later.
  */
-public class FreeParkingSquare {
+public class FreeParkingSquare extends BoardSquare {
+
+    /*****************************************************************
+     * Constructor instantiates the variables and sets the name of the
+     * FreeParkingSquare to what is input as a parameter.
+     *
+     * @param name String that will be the name of the BoardSquare.
+     ****************************************************************/
+    public FreeParkingSquare(String name) {
+        super(name);
+    }
 }
