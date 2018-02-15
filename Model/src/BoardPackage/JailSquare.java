7f88e33e8c6469978ea5c9d572de510277c761ba
David <bcdave.301@gmail.com>
2018-02-15 10:17:22 -0500

    Unfinished 1

--- Model/src/BoardPackage/JailSquare.java
+++ Model/src/BoardPackage/JailSquare.java
@@ -1,7 +1,32 @@
 package BoardPackage;
 
-/**
- * Created by Dylan Kernohan on 2/10/18.
- */
-public class JailSquare {
+import GamePackage.Player;
+
+import java.util.ArrayList;
+
+/*********************************************************************
+ * The JailSquare Class tracks what players are in jail.
+ *
+ * @author David Baas
+ * @version 2/14/2018
+ ********************************************************************/
+public class JailSquare extends BoardSquare {
+
+    /**An ArrayList to store what players are in jail.*/
+    private ArrayList<Player> inJail;
+
+    /*****************************************************************
+     * The default constructor creates a JailSquare setting its name
+     * variable and initializes the inJail ArrayList.
+     *
+     * @param name sets the name of the board square.
+     ****************************************************************/
+    public JailSquare(String name){
+
+        super(name);
+
+        inJail = new ArrayList<Player>();
+    }
+
+
 }
