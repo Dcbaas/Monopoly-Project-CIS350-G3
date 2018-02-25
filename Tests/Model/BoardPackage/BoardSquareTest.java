82f6339fce396a06d778c022a6d98380c8e2cf85
Unknown <kernohad@mail.gvsu.edu>
2018-02-24 19:38:18 -0500

    created BoardSquareTest class

---
+++ Tests/Model/BoardPackage/BoardSquareTest.java
@@ -0,0 +1,44 @@
+package Model.BoardPackage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BoardSquareTest {
+
+    @Test
+    public void getNameTest() {
+    }
+
+    @Test
+    public void setNameTest() {
+    }
+
+    @Test
+    public void getOccupiedByTest() {
+    }
+
+    @Test
+    public void setOccupiedByTest() {
+    }
+
+    @Test
+    public void addPlayerTest() {
+    }
+
+    @Test
+    public void removePlayerTest() {
+    }
+
+    @Test
+    public void getPOSITIONTest() {
+    }
+
+    @Test
+    public void getTypeTest() {
+    }
+
+    @Test
+    public void setTypeTest() {
+    }
+}
