da48033e3b04ea72724188ec2c8313ae99958d84
David <bcdave.301@gmail.com>
2018-03-26 21:50:28 -0400

    Wrote Comments and formatted

--- src/View/TaxSpace.java
+++ src/View/TaxSpace.java
@@ -25,16 +25,38 @@ public class TaxSpace extends JPanel {
    */
   private static final int HEIGHT = 400;
 
-  private JLabel taxName;
-
-  private JLabel taxInfo;
-
+  /**
+   * An array of infomation strings for taxInfo.
+   */
   private static final String[] infoStrings = {"Pay 10% or $200", "Pay $75.00"};
 
+  /**
+   * A JLabel to track the name of the TaxSpace.
+   */
+  private JLabel taxName;
+
+  /**
+   * A JLabel to display info about the tax in the TaxSpace.
+   */
+  private JLabel taxInfo;
+
+  /**
+   * A boolean to track what type of tax space this TaxSpace is.
+   */
   private boolean incomeTax;
 
+  /**
+   * A Dimension to track the dimensions of the tax space.
+   */
   private Dimension dimension;
 
+  /********************************************************************
+   * The constructor for the TaxSpace creates a tax space depanding on
+   * weather it is an income tax space or a luxury space.
+   *
+   * @param incomeTax Determines what type of tax space this TaxSpace
+   * is.
+   *******************************************************************/
   public TaxSpace(boolean incomeTax) {
     this.incomeTax = incomeTax;
 
@@ -45,12 +67,17 @@ public class TaxSpace extends JPanel {
     } else {
       taxName = new JLabel("Luxury Tax");
       taxInfo = new JLabel(infoStrings[1]);
+      dimension = new Dimension(HEIGHT, WIDTH);
     }
 
     setLayout(new GridBagLayout());
     drawLabels();
   }
 
+  /********************************************************************
+   * The drawLabels method is a private method that draws the
+   * labels onto the JPanel.
+   *******************************************************************/
   private void drawLabels() {
     GridBagConstraints g = new GridBagConstraints();
     g.gridx = 0;
