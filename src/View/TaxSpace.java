4c19463b8795d6ad2668ba4a57a71ae28a9e7317
David <bcdave.301@gmail.com>
2018-03-26 21:02:54 -0400

    Created TaxSpace

---
+++ src/View/TaxSpace.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.JPanel;
+
+/**********************************************************************
+ * The TaxSpace creates a board space that is a tax square on the board
+ * of Monopoly
+ *
+ * @author David Baas
+ * @version 3/26/2018
+ *********************************************************************/
+public class TaxSpace extends JPanel {
+
+}
