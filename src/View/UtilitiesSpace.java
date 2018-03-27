241de02b7ea2d1b3414b7b0461ab2575ba6b3a46
David <bcdave.301@gmail.com>
2018-03-26 20:00:37 -0400

    Wrote Utilities Class.

--- src/View/UtilitiesSpace.java
+++ src/View/UtilitiesSpace.java
@@ -1,10 +1,14 @@
 package View;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class UtilitiesSpace extends JPanel {
@@ -21,17 +25,52 @@ public class UtilitiesSpace extends JPanel {
 
   private static Image utilitiesImg;
 
+  private JLabel name;
+
+  private static JLabel price = new JLabel("$150");
+
   private Dimension dimension;
 
+  private boolean waterworks;
+
   public UtilitiesSpace(boolean waterworks) throws IOException {
+    this.waterworks = waterworks;
+
     if (waterworks) {
       dimension = new Dimension(WIDTH, HEIGHT);
       utilitiesImg = ImageIO.read(new File("res/waterWorks.png"));
+      name = new JLabel("Water Works");
     } else {
       dimension = new Dimension(HEIGHT, WIDTH);
       utilitiesImg = ImageIO.read(new File("res/electricCompany.jpg"));
+      name = new JLabel("Electric Company");
+    }
+    setLayout(new GridBagLayout());
+  }
+
+  private void drawLabels() {
+    GridBagConstraints g = new GridBagConstraints();
+    g.gridx = 0;
+    g.gridy = 0;
+    g.anchor = GridBagConstraints.NORTH;
+    g.weighty = 2;
+    add(name, g);
+
+    g = new GridBagConstraints();
+    g.gridx = 0;
+    g.gridy = 2;
+    g.anchor = GridBagConstraints.SOUTH;
+    add(price, g);
+  }
+
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    if (waterworks) {
+      g.drawImage(utilitiesImg, 20, 50, 100, 100, null);
+    } else {
+      g.drawImage(utilitiesImg, 20, 30, 80, 130, null);
     }
   }
 
-  
+
 }
