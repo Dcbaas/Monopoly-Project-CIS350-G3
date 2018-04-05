9fb7eb773bf6425f2ad71c6addb956a79953cea5
Dcbaas <Dcbaas@github.com>
2018-04-04 21:27:20 -0400

    Wrote and formatted Comments for TextPanel.

--- src/View/TextPanel.java
+++ src/View/TextPanel.java
@@ -3,20 +3,33 @@ package View;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
+/**********************************************************************
+ * The Text Panel creates a JPanel with the ability to dispaly
+ * information to the user using text while also allowing for text input.
+ *
+ * @author David Baas
+ * @version 4/4/2018
+ *********************************************************************/
 public class TextPanel extends JPanel {
 
+  /**
+   * A JTextArea to display information.
+   */
   private JTextArea txtArea;
 
+  /**A JTextField to get input.*/
   private JTextField cmd;
 
+  /********************************************************************
+   * The constructor initializes the TextPanes and positions them on
+   * the JPanel.
+   *******************************************************************/
   public TextPanel() {
     txtArea = new JTextArea();
     cmd = new JTextField();
@@ -26,13 +39,17 @@ public class TextPanel extends JPanel {
     setLayout(new BorderLayout());
 
     add(txtArea,BorderLayout.CENTER);
-
     add(cmd,BorderLayout.SOUTH);
 
     Border blackLine = BorderFactory.createLineBorder(Color.BLACK);
     setBorder(blackLine);
   }
 
+  /********************************************************************
+   * Get the minimum dimension of this TextPanel.
+   *
+   * @return The minimum dimension of this TextPanel.
+   *******************************************************************/
   public Dimension getMinimumSize(){
     return new Dimension(500,540);
   }
