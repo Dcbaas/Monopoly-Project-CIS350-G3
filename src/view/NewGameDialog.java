0c679216f05e5b60923f35dbc04cd0bfed2ad097
Dcbaas <Dcbaas@github.com>
2018-04-16 00:31:18 -0400

    Reformatted GameController to comply with Google checks.

--- src/view/NewGameDialog.java
+++ src/view/NewGameDialog.java
@@ -172,8 +172,9 @@ public class NewGameDialog extends JDialog {
         }
       }
     }
-    if(parentArrayList.size() < 2)
+    if (parentArrayList.size() < 2) {
       approved = false;
+    }
     //Was there an invalid input. If yes force error correction.
     if (approved) {
       dispose();
@@ -186,6 +187,7 @@ public class NewGameDialog extends JDialog {
 
   /**
    * Returns the ArrayList of generated Players.
+   *
    * @return The arrayList of players.
    */
   public ArrayList<Player> getPlayers() {
